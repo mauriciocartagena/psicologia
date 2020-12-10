e859e9b3a8f7758cee70318cdac4546d34d06c82
Mauricio Cartagena <mc973220@gmail.com>
2020-12-09 20:36:23 -0400

    added on aside Institution

--- src/components/main/Aside.js
+++ src/components/main/Aside.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUniversity } from '@fortawesome/free-solid-svg-icons'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { startLogout } from '../../actions/auth';
 import '../../styles/styles.css';
 
@@ -12,7 +12,7 @@ export const Aside = () => {
     const dispatch = useDispatch();
 
     const { uiSidebar } = useSelector(state => state.ui);
-
+    
     const handleLogout = () => {
         dispatch( startLogout() );
     }
@@ -26,18 +26,21 @@ export const Aside = () => {
             <div className="leftside-navigation">
                 <ul className="sidebar-menu" id="nav-accordion">
                     <li className="sub-menu">
-                        <Link to='/user' >
-                            <i _ngcontent-tkb-c31="" className="fa fa-user"></i>
+                        <NavLink to='/user' activeClassName="active" >
+                            <i className="fa fa-user"></i>
                             <span>Perfil</span>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li className="sub-menu">
-                        <Link to='/user' >
-                            <span> <FontAwesomeIcon icon={ faUniversity } /> Institución</span>
-                        </Link>
+                         <NavLink  to="/institution">
+                            <i>
+                                <FontAwesomeIcon icon={ faUniversity } />
+                            </i> 
+                            <span>Institución</span>
+                        </NavLink>
                     </li>
                     <li onClick={ handleLogout } >
-                        <a className='hyper' >
+                        <a className='hyper'>
                             <i className="fa fa-sign-out"></i>
                             <span>Cerrar Sesión</span>
                         </a>

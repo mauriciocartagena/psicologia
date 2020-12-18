c95a60432d171503e538b65fefb54a5efe877ac2
Mauricio Cartagena <mc973220@gmail.com>
2020-12-18 02:51:08 -0400

    added informatión of test

--- src/components/test/TestScreen.js
+++ src/components/test/TestScreen.js
@@ -4,32 +4,68 @@ import { faBong, faCubes } from '@fortawesome/free-solid-svg-icons'
 
 export const TestScreen = () => {
     return (
-        <div className="row" >
-            <div className="col-sm-6" > 
-                <h3>
-                    <i>
-                        <FontAwesomeIcon icon={ faBong } />
-                        <span> Pruebas Simples</span>
-                    </i>
-                </h3>
-                <p>
-                    Pruebas simples sin graficos, solamente preguntas.
-                </p>
-                <button className="btn btn-primary btn-lg btn-block" >Elegir</button>
+        <section >
+            <div className="row" >
+                <div className="col-sm-4">
+                    <div className="mini-stat clearfix">
+                        <span className="mini-stat-icon tar">
+                            <i className="fa fa-tag"></i>
+                        </span>
+                        <div class="mini-stat-info">
+                            <span>31</span>
+                             Cantidad de Pruebas simples 
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="mini-stat clearfix">
+                        <span className="mini-stat-icon pink">
+                            <i className="fa fa-tag"></i>
+                        </span>
+                        <div class="mini-stat-info">
+                            <span>60</span>
+                             Cantidad total de pruebas 
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="mini-stat clearfix">
+                        <span className="mini-stat-icon orange">
+                            <i className="fa fa-tag"></i>
+                        </span>
+                        <div class="mini-stat-info">
+                            <span>29</span>
+                             Cantidad de Pruebas de formas 
+                        </div>
+                    </div>
+                </div>
+            </div>   
+            <div className="row" >
+                <div className="col-sm-6" > 
+                    <h2>
+                        <i>
+                            <FontAwesomeIcon icon={ faBong } />
+                            <span> Pruebas Simples</span>
+                        </i>
+                    </h2>
+                    <p>
+                        Pruebas simples sin graficos, solamente preguntas.
+                    </p>
+                    <button className="btn btn-primary btn-lg btn-block" >Elegir</button>
+                </div>
+                <div className="col-sm-6">
+                    <h2>
+                        <i>
+                            <FontAwesomeIcon icon={ faCubes } />
+                            <span> Pruebas de Formas</span>
+                        </i>
+                    </h2>
+                    <p>
+                        Pruebas con graficos, formas entre otras cosas.
+                    </p>
+                    <button className="btn btn-info btn-lg btn-block" >Elegir</button>
+                </div>
             </div>
-            <div className="col-sm-6">
-                <h3>
-                    <i>
-                        <FontAwesomeIcon icon={ faCubes } />
-                        <span> Pruebas de Formas</span>
-                    </i>
-                </h3>
-                <p>
-                    Pruebas con graficos, formas entre otras cosas.
-                </p>
-                <button className="btn btn-info btn-lg btn-block" >Elegir</button>
-            </div>
-        </div>
-
+        </section>
     )
 }

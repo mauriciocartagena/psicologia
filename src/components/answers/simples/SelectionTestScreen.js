8ad9f74ddd9c02a17e9022858c1a11526b2f8186
Mauricio Cartagena <mc973220@gmail.com>
2021-01-18 14:39:37 -0400

    Created screen selection test

--- src/components/answers/simples/SelectionTestScreen.js
+++ src/components/answers/simples/SelectionTestScreen.js
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useFetchTestSimple } from '../../../hooks/QuestionSimple/useFetchTestSimple';
+import { useForm } from '../../../hooks/useForm';
 
 export const SelectionTestScreen = () => {
+
+    const [ state, setState ] = useState([]);
+
+    const { questionsSimple } = useFetchTestSimple();
+
+    const [ selectionDefault, setSelectionDefault ] = useState('');
+
+    const [ formSelectionInputValues, handleSelectionInputValueChange ] = useForm({
+        tSimple: selectionDefault
+    });
+
+    const  { tSimple } = formSelectionInputValues;
+
+    const handleSubmitForm = ( e ) => {
+        e.preventDefault();
+        if ( tSimple === '' ) {
+            console.log( selectionDefault );
+        }
+    }
+
+    useEffect(() => {
+        
+        if ( questionsSimple !== [] ) {
+            if ( questionsSimple[0] !== undefined ) {   
+                
+                const DEFAULTVALUE = questionsSimple[0].id_test;
+                setSelectionDefault( DEFAULTVALUE );
+
+            }
+        }
+        
+        setState( questionsSimple );
+        
+    }, [ questionsSimple ]);
+
     return (
         <div className="row">
             <div className="col-sm-12  animated fadeIn">
@@ -10,21 +47,27 @@ export const SelectionTestScreen = () => {
                     </header>
                     <div className="panel-body">
                         <div className="position-center">
-                            <form>
+                            <form onSubmit={ handleSubmitForm } >
                                 <div className="form-group">
-                                    <label>Prueba</label>
-                                    <p>Prueba Seleccionada</p>
+                                    <label>Prueba Simple</label>
                                     <select 
-                                        formcontrolname="test" 
-                                        name="test"
+                                        formcontrolname="test-simple" 
+                                        name="tSimple"
+                                        value={ tSimple }
+                                        onChange={ handleSelectionInputValueChange }   
                                         className="form-control ng-valid ng-dirty ng-touched"
                                         >
-                                        <option>Prueba 1</option>
-                                        <option>Prueba 2</option>
-                                        <option>Prueba 3</option>
+                                        {
+                                            state.map(( e )=>(
+                                                <option 
+                                                    key={ e.id_test } 
+                                                    value={ e.id_test } 
+                                                >{ e.nombre_test }</option>
+                                            ))
+                                        }
                                     </select>
                                 </div>
-                                <button className="btn btn-primary btn-round btn-block" >Continuar</button>
+                                <button className="btn btn-primary btn-round btn-block">Continuar</button>
                             </form>
                         </div>
                     </div>

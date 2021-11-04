8f39c756bcf87fa411c6afe49dd521ca8688868e
Mauricio Cartagena <mc973220@gmail.com>
2021-11-04 13:58:23 -0400

    Se realizo la validacion si hay datos o no

--- src/actions/questionSimple.js
+++ src/actions/questionSimple.js
@@ -3,19 +3,17 @@ import { uiCloseLoadingSaveButton, uiFalseDisabledButton, uiOpenLoadingSaveButto
 import { fetchConToken } from "../helpers/fetch";
 import { types } from '../types/types';
 
-export const simpleRegister = ( pregunta, id_category, id_test ) => {
+export const simpleRegister = (pregunta, id_category, id_test) => {
 
-    return async ( dispatch ) => {
+    return async (dispatch) => {
 
-       console.log(pregunta, id_category, id_test)
-
-        dispatch( uiCloseLoadingSaveButton() );
-        dispatch( uiTrueDisabledButton() );
-        if ( pregunta === '' || id_test === '' || id_category === '' || pregunta === undefined) {
+        dispatch(uiCloseLoadingSaveButton());
+        dispatch(uiTrueDisabledButton());
+        if (pregunta === '' || id_test === '' || id_category === '' || pregunta === undefined) {
             return (
-                Swal.fire(':(', 'Es requerido todos los campos.','error'),
-                dispatch( uiOpenLoadingSaveButton() ),
-                dispatch( uiFalseDisabledButton () )
+                Swal.fire(':(', 'Es requerido todos los campos.', 'error'),
+                dispatch(uiOpenLoadingSaveButton()),
+                dispatch(uiFalseDisabledButton())
             )
         }
 
@@ -25,43 +23,43 @@ export const simpleRegister = ( pregunta, id_category, id_test ) => {
                 pregunta: pregunta,
                 id_categoria: id_category,
                 id_test: id_test
-            },'POST');
+            }, 'POST');
 
             const body = await resp.json();
 
-            if ( body.ok ) {
-                Swal.fire(':)','Pregunta registrada', 'success');
-                dispatch( uiOpenLoadingSaveButton() );
-                dispatch( uiFalseDisabledButton () );
-            }else {  
+            if (body.ok) {
+                Swal.fire(':)', 'Pregunta registrada', 'success');
+                dispatch(uiOpenLoadingSaveButton());
+                dispatch(uiFalseDisabledButton());
+            } else {
 
                 Swal.fire(':(', body.msg, 'error');
-                dispatch( uiOpenLoadingSaveButton() );
-                dispatch( uiFalseDisabledButton () );
+                dispatch(uiOpenLoadingSaveButton());
+                dispatch(uiFalseDisabledButton());
             }
-            
+
         } catch (error) {
-            console.log( error);
-            dispatch( uiOpenLoadingSaveButton() );
-            dispatch( uiFalseDisabledButton () );
+            console.log(error);
+            dispatch(uiOpenLoadingSaveButton());
+            dispatch(uiFalseDisabledButton());
         }
 
     }
 
 }
 
-export const simpleEdit = ( id_pregunta, pregunta, id_categoria, id_test ) => {
+export const simpleEdit = (id_pregunta, pregunta, id_categoria, id_test) => {
 
-    return async ( dispatch ) => {
+    return async (dispatch) => {
 
-        dispatch( uiCloseLoadingSaveButton() );
-        dispatch( uiTrueDisabledButton() );
+        dispatch(uiCloseLoadingSaveButton());
+        dispatch(uiTrueDisabledButton());
 
-        if ( id_pregunta === '' || pregunta === '' || id_test === '' || id_categoria === '' || pregunta === undefined) {
+        if (id_pregunta === '' || pregunta === '' || id_test === '' || id_categoria === '' || pregunta === undefined) {
             return (
-                Swal.fire(':(', 'Es requerido todos los campos.','error'),
-                dispatch( uiOpenLoadingSaveButton() ),
-                dispatch( uiFalseDisabledButton () )
+                Swal.fire(':(', 'Es requerido todos los campos.', 'error'),
+                dispatch(uiOpenLoadingSaveButton()),
+                dispatch(uiFalseDisabledButton())
             )
         }
 
@@ -69,65 +67,65 @@ export const simpleEdit = ( id_pregunta, pregunta, id_categoria, id_test ) => {
 
             const resp = await fetchConToken('pregunta-simple/update', {
                 id_pregunta,
-                pregunta:pregunta,
-                id_test:id_test,
-                id_categoria:id_categoria
-            },'PUT');
+                pregunta: pregunta,
+                id_test: id_test,
+                id_categoria: id_categoria
+            }, 'PUT');
 
             const body = await resp.json();
 
-            if ( body.ok ) {
-                Swal.fire(':)','Pregunta Modificada', 'success');
-                dispatch( uiOpenLoadingSaveButton() );
-                dispatch( uiFalseDisabledButton () );
-            }else {  
+            if (body.ok) {
+                Swal.fire(':)', 'Pregunta Modificada', 'success');
+                dispatch(uiOpenLoadingSaveButton());
+                dispatch(uiFalseDisabledButton());
+            } else {
 
                 Swal.fire(':(', body.msg, 'error');
-                dispatch( uiOpenLoadingSaveButton() );
-                dispatch( uiFalseDisabledButton () );
+                dispatch(uiOpenLoadingSaveButton());
+                dispatch(uiFalseDisabledButton());
             }
-            
+
         } catch (error) {
-            console.log( error);
-            dispatch( uiOpenLoadingSaveButton() );
-            dispatch( uiFalseDisabledButton () );
+            console.log(error);
+            dispatch(uiOpenLoadingSaveButton());
+            dispatch(uiFalseDisabledButton());
         }
-        
+
 
     }
 }
 
-export const questionDeleteSimple = ( id_pregunta ) => {
+export const questionDeleteSimple = (id_pregunta) => {
 
     return async () => {
         try {
 
             const resp = await fetchConToken('pregunta-simple/delete', {
                 id_pregunta
-            },'DELETE');
+            }, 'DELETE');
 
             const body = await resp.json();
 
-            if ( body.ok ) {
-                Swal.fire(':)','Pregunta Eliminada', 'success');
-            }else {  
+            if (body.ok) {
+                Swal.fire(':)', 'Pregunta Eliminada', 'success');
+            } else {
 
                 Swal.fire(':(', body.msg, 'error');
             }
-            
+
         } catch (error) {
-            console.log( error);
+            console.log(error);
         }
 
     }
 
 }
 
-export const questionSetSimple = ( id_pregunta, pregunta, id_categoria, id_test ) => ( questionSimple( { id_pregunta, pregunta, id_categoria, id_test }  ) );
+export const questionSetSimple = (id_pregunta, pregunta, id_categoria, id_test) => (questionSimple({ id_pregunta, pregunta, id_categoria, id_test }));
 
-const questionSimple = ( questionActive ) => ({ 
+const questionSimple = (questionActive) => ({
     type: types.questionSimpleSetActive,
     payload: questionActive
 });
 
-export const questionClearSimple = ()  =>({ type: types.questionSimpleClearActive });
+export const questionClearSimple = () => ({ type: types.questionSimpleClearActive });
--- src/components/test/simple/SimpleScreenRegister.js
+++ src/components/test/simple/SimpleScreenRegister.js
@@ -11,43 +11,44 @@ export const SimpleScreenRegister = () => {
     const dispatch = useDispatch();
 
     const { questionsSimple } = useFetchTestSimple();
+
     const { data } = useFetchCategory();
 
-    const [ state, setState ] = useState([]);
-    const [ categoryData, setCategoryData ] = useState([]);
-    const [ testName, setTestName ] = useState([]);
-    const [ nameCategory, setNameCategory ] = useState([]);
+    const [state, setState] = useState([]);
+    const [categoryData, setCategoryData] = useState([]);
+    const [testName, setTestName] = useState([]);
+    const [nameCategory, setNameCategory] = useState([]);
 
-    const [ formQuestionSimpleInputValues, handleQuestionSimpleInputValueChange ] = useForm({
+    const [formQuestionSimpleInputValues, handleQuestionSimpleInputValueChange] = useForm({
         question: '',
         tSimple: '',
         category: ''
     });
 
-    const  { question, tSimple, category } = formQuestionSimpleInputValues;
+    const { question, tSimple, category } = formQuestionSimpleInputValues;
 
-    const handleRegister = ( e ) =>{
+    const handleRegister = (e) => {
         e.preventDefault();
-        if ( question !== '' && tSimple !== '' && category !== '' ) {
-            dispatch( simpleRegister( question, tSimple, category ) );
+        if (question !== '' && tSimple !== '' && category !== '') {
+            dispatch(simpleRegister(question, tSimple, category));
         }
-        Swal.fire(':(','Todos los campos son necesarios verificar nuevamente', 'error');
+        Swal.fire(':(', 'Todos los campos son necesarios verificar nuevamente', 'error');
     }
 
     useEffect(() => {
-        
-        setState( questionsSimple );
-        setCategoryData( data );
-        
-    }, [ questionsSimple, data ]);
 
-    
+        setState(questionsSimple);
+        setCategoryData(data);
+
+    }, [questionsSimple, data]);
+
+
     useEffect(() => {
-        
-        setNameCategory( data.find( e => e.id_categoria.toString() === category ) );
-        setTestName( questionsSimple.find( e => e.id_test.toString() === tSimple ) );
 
-    }, [ category, tSimple, data, questionsSimple ]);
+        setNameCategory(data.find(e => e.id_categoria.toString() === category));
+        setTestName(questionsSimple.find(e => e.id_test.toString() === tSimple));
+
+    }, [category, tSimple, data, questionsSimple]);
 
     return (
         <div className="row">
@@ -56,67 +57,87 @@ export const SimpleScreenRegister = () => {
                     <header className="panel-heading">
                         REGISTRO DE PRUEBAS SIMPLES
                     </header>
-                
-                        <div className="panel-body">
-                            <div className="position-center">
-                                <form onSubmit={ handleRegister }>
-                                <div className="form-group">
-                                    <label>Pregunta</label>
-                                    <input 
-                                        type="text" 
-                                        className="form-control" 
-                                        id="InputQuestion" 
-                                        name="question"
-                                        onChange={ handleQuestionSimpleInputValueChange } 
-                                        value={ question }
-                                        placeholder="Ingrese la pregunta" />
-                                </div>
-                                <div className="form-group">
-                                    <label>Prueba Simple</label>
-                                    <p>Prueba seleccionada : `{ ( testName !== undefined || '' ? testName.nombre_test : '') }`</p>
-                                    <select 
-                                        formcontrolname="test-simple" 
-                                        name="tSimple"
-                                        onChange={ handleQuestionSimpleInputValueChange } 
-                                        className="form-control ng-valid ng-dirty ng-touched"
-                                        value={ tSimple  }
-                                        >
-                                        {
-                                            state.map(( e )=>(
-                                                <option 
-                                                    key={ e.id_test } 
-                                                    value={ e.id_test } 
-                                                >{ e.nombre_test }</option>
-                                            ))
-                                        }
-                                    </select>
-                                </div>
-                                <div className="form-group">
-                                    <label>Categoria</label>
-                                    <p>Categoria seleccionada : `{ ( nameCategory !== undefined || '' ? nameCategory.nombre_categoria : '') }`</p>
-                                    <select 
-                                        formcontrolname="curso" 
-                                        name="category"
-                                        onChange={ handleQuestionSimpleInputValueChange } 
-                                        className="form-control ng-valid ng-dirty ng-touched"
-                                        value={ category  }
-                                        >
-                                        {
-                                            categoryData.map(( e )=>(
-                                                <option 
-                                                    key={ e.id_categoria } 
-                                                    value={ e.id_categoria } 
-                                                >{ e.nombre_categoria }</option>
-                                            ))
-                                        }
-                                    </select>
-                                </div>
-                                <p className="help-block">Todos los campos son requeridos.</p>
-                                <button type="submit" className="btn btn-success">Registrar</button>
-                            </form>
-                            </div>
+
+                    <div className="panel-body">
+                        <div className="position-center">
+
+                            {
+
+
+                                questionsSimple.length !== 0 ?
+
+
+
+                                    <form onSubmit={handleRegister}>
+                                        <div className="form-group">
+                                            <label>Pregunta</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                id="InputQuestion"
+                                                name="question"
+                                                onChange={handleQuestionSimpleInputValueChange}
+                                                value={question}
+                                                placeholder="Ingrese la pregunta" />
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label>Prueba Simple</label>
+                                            <p>Prueba seleccionada : `{(testName !== undefined || '' ? testName.nombre_test : '')}`</p>
+
+                                            <select
+                                                formcontrolname="test-simple"
+                                                name="tSimple"
+                                                onChange={handleQuestionSimpleInputValueChange}
+                                                className="form-control ng-valid ng-dirty ng-touched"
+                                                value={tSimple}
+                                            >
+                                                {
+
+                                                    state.map((e) => (
+                                                        <option
+                                                            key={e.id_test}
+                                                            value={e.id_test}
+                                                        >
+                                                            {
+                                                                e.nombre_test
+                                                            }
+                                                        </option>
+                                                    ))
+                                                }
+                                            </select>
+
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Categoria</label>
+                                            <p>Categoria seleccionada : `{(nameCategory !== undefined || '' ? nameCategory.nombre_categoria : '')}`</p>
+                                            <select
+                                                formcontrolname="curso"
+                                                name="category"
+                                                onChange={handleQuestionSimpleInputValueChange}
+                                                className="form-control ng-valid ng-dirty ng-touched"
+                                                value={category}
+                                            >
+                                                {
+                                                    categoryData.map((e) => (
+                                                        <option
+                                                            key={e.id_categoria}
+                                                            value={e.id_categoria}
+                                                        >{e.nombre_categoria}</option>
+                                                    ))
+                                                }
+                                            </select>
+                                        </div>
+                                        <p className="help-block">Todos los campos son requeridos.</p>
+                                        <button type="submit" className="btn btn-success">Registrar</button>
+                                    </form>
+
+                                    : <div>cargando ...</div>
+                            }
 
                         </div>
+
+                    </div>
                 </section>
             </div>
         </div>
